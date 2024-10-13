6c41fa4ae15fe01e12b6005f41555cd196c30089
Camber <cedge2@asu.edu>
2024-10-13 13:14:56 -0500

    GET conversations + messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cookieParser());
@@ -16,8 +18,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
 });
 
 //TODO: add socket.io to server
